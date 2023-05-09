c9a968ab892bb8f1d55a748fb7afdd2966891efa
Salim Goudarzi <82885835+SalimGoudarzi@users.noreply.github.com>
2023-05-09 13:31:58 +0100

    Update initialiseSYS.m
    Signed-off-by: Salim Goudarzi <82885835+SalimGoudarzi@users.noreply.github.com>

--- initialiseSYS.m
+++ initialiseSYS.m
@@ -1,10 +1,12 @@
 %**************************************************************************
-function [Sx,Su,Sw]=initialiseSYS(Nc)
+function [Sx,Su,Sw,Hmax]=initialiseSYS(Nc,phi,d)
+%maximum subsurface storage
+Hmax  = phi*d;
 %--------------------------------------------------------------------------
 %                            initialise variables
 %--------------------------------------------------------------------------
 Sx = zeros(Nc,1)+eps;
 %maximum subsurface flow per unit area
-Sw = zeros(Nc,1)+eps;
+Sw = zeros(Nc,1)+ Hmax;
 %unsaturated zone initial storage 
 Su = Sw*0+eps;
